008cadd611c283aaa17c77a931773dab945c4ba4
WaldesJr <waldes.junior@gmail.com>
2025-09-10 11:32:58 -0300

    Funções adionar amigo e limpar campo

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    //document.getElementsByClassName('button-add').disable = true;
+    let nomeAmigo = document.querySelector('input').value;
+    
+    if (nomeAmigo == '') {
+        alert('Por favor, você precisa digitar um nome!');
+    } else {
+        listaAmigos.push(nomeAmigo);
+        console.log(listaAmigos);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
+
+// function reiniciarJogo() {
+//     numeroSecreto = gerarNumeroAleatorio();
+//     limparCampo();
+//     tentativas = 1;
+//     exibirMensagemInicial();
+//     document.getElementById('reiniciar').setAttribute('disabled', true)
+// }
+
+// function gerarNumeroAleatorio() {
+//     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+//     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+//     if (quantidadeDeElementosNaLista == numeroLimite) {
+//         listaDeNumerosSorteados = [];
+//     }
+//     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+//         return gerarNumeroAleatorio();
+//     } else {
+//         listaDeNumerosSorteados.push(numeroEscolhido);
+//         console.log(listaDeNumerosSorteados)
+//         return numeroEscolhido;
+//     }
+// }
